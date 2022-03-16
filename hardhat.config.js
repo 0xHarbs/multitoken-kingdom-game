4d6c0217575d73900184ac3fb49eccbc3bd31fbb
James Harbridge <jamesharbridge@outlook.com>
2022-03-16 18:59:03 +0000

    Uncommented deploy and updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,10 @@ const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
-  // networks: {
-  //   rinkeby: {
-  //     url: ALCHEMY_API_KEY_URL,
-  //     accounts: [RINKEBY_PRIVATE_KEY],
-  //   },
-  // },
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
 };
